f8cf18b712c60772c5c86caaf3160667e288780c
K <12345665@qq.com>
2023-01-16 01:44:12 -0500

    get api

--- routes/apidemo.js
+++ routes/apidemo.js
@@ -23,50 +23,59 @@ var api=  function(val){
 }
 
 
-router.get('/test', async function (req, res, next) {
-    res.render('pages/apitest', {title: 'apitest'});
+router.get('/', async function (req, res, next) {
+    res.render('pages/apidemo', {title: 'apitest'});
 });
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-    // res.render('index', { title: 'Express' });
-
-
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
-        temperature: 0.5,
-        max_tokens: 4000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-        stop: ["You:"],
-    });
-
-    console.log('response----',response.data.choices[0].text);
-      res.send( '2223');
-
-    // let msg =     await openai.createCompletion({
-    //     model: "text-davinci-003",
-    //     prompt: " \n \n ",
-    //     temperature: 0.7,
-    //     max_tokens: 4000,
-    //     top_p: 1,
-    //     frequency_penalty: 0,
-    //     presence_penalty: 0,
-    // });
-    // console.log(msg);
-    // res.send(msg);
-
-});
 
 
 
 //使用POST API
-router.post('/wukong1',   (req, res) => {
+router.get('/wukong1',   (req, res) => {
 
     try {
-        const data = req.body.message
-        // const data = req.query.message
+        // const data = req.body.message
+        const data = req.query.message
+        console.log('收到数据', data);
+        // res.send('Data received:' + data)
+
+        openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: data,
+            temperature: 0.5,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+            stop: ["You:"],
+        }).then((response) => {
+            console.log('response----',response );
+            try {
+                var msg = response.data.choices[0].text;
+                console.log(msg);
+                res.send(msg);
+            } catch (e) {
+
+                res.send('error')
+            }
+        }).catch((error) => {
+            console.log(error);
+            res.send('error:'+ error)
+        });
+    } catch (e) {
+        res.send('error')
+    }
+
+})
+
+
+
+//使用POST API
+router.post('/wukong2',   (req, res) => {
+
+    try {
+        // const data = req.body.message
+        const data = req.query.message
         console.log('收到数据', data);
         // res.send('Data received:' + data)
 
