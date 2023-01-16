2ed03d28c86acc98b2732b4d4419607c116fc884
K <12345665@qq.com>
2023-01-16 08:26:31 -0500

    fix

--- routes/apidemo.js
+++ routes/apidemo.js
@@ -1,12 +1,14 @@
 
 
 // import { Configuration, OpenAIApi } from "openai";
+const apikey1 ='apikey1'
+const apikey2 ='apikey2'
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
     organization: "org-nzotoj7uNoUKh3tDk16xGrpd",
-    apiKey: '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',
+    apiKey: apikey2,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -36,7 +38,7 @@ router.get('/wukong1',   (req, res) => {
     try {
         // const data = req.body.message
         const data = req.query.message
-        console.log('收到数据', data);
+        console.log('收到GET数据: ', data);
         // res.send('Data received:' + data)
 
         openai.createCompletion({
@@ -49,21 +51,22 @@ router.get('/wukong1',   (req, res) => {
             presence_penalty: 0,
             stop: ["You:"],
         }).then((response) => {
-            console.log('response----',response );
+            // console.log('response----',response );
             try {
                 var msg = response.data.choices[0].text;
-                console.log(msg);
+                console.log('输出数据: ',msg);
                 res.send(msg);
             } catch (e) {
-
-                res.send('error')
+                console.log('error,输出error1: ',e);
+                res.send(e)
             }
-        }).catch((error) => {
-            console.log(error);
-            res.send('error:'+ error)
+        }).catch((e) => {
+            console.log('error,输出error2: ',e);
+            res.send(e)
         });
     } catch (e) {
-        res.send('error')
+        console.log('error,输出error3: ',e)
+        res.send(e)
     }
 
 })
@@ -74,9 +77,11 @@ router.get('/wukong1',   (req, res) => {
 router.post('/wukong2',   (req, res) => {
 
     try {
-        // const data = req.body.message
-        const data = req.query.message
-        console.log('收到数据', data);
+        //post body
+        const data = req.body.message
+        //get query
+        // const data = req.query.message
+        console.log('收到POST数据: ', data);
         // res.send('Data received:' + data)
 
         openai.createCompletion({
@@ -89,21 +94,22 @@ router.post('/wukong2',   (req, res) => {
             presence_penalty: 0,
             stop: ["You:"],
         }).then((response) => {
-            console.log('response----',response );
+            // console.log('response----',response );
             try {
                 var msg = response.data.choices[0].text;
-                console.log(msg);
+                console.log('输出数据: ',msg);
                 res.send(msg);
             } catch (e) {
-
-                res.send('error')
+                console.log('error,输出error1: ',e);
+                res.send(e)
             }
-        }).catch((error) => {
-            console.log(error);
-            res.send('error:'+ error)
+        }).catch((e) => {
+            console.log('error,输出error2: ',e);
+            res.send(e)
         });
     } catch (e) {
-        res.send('error')
+        console.log('error,输出error3: ',e)
+        res.send(e)
     }
 
 })
