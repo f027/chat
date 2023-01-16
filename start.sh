c2c96380a545fff4eea8ebfed8fc1e264eb42ee5
K <12345665@qq.com>
2023-01-16 09:52:07 -0500

    update  stop.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-./stop.sh
+# Find the process ID for port 3002
+port=3002
+#根据端口号查询对应的pid
+pid=$(netstat -nlp | grep :3002 | awk '{print $7}' | awk -F"/" '{ print $1 }');
+
+#杀掉对应的进程，如果pid不存在，则不执行
+if [  -n  "$pid"  ];  then
+    echo "kill pid $pid"
+    kill  -9  $pid;
+fi
+
 
 nohup node ./bin/www >chat.log 2>&1 &
 
--- stop.sh
+++ stop.sh
@@ -3,7 +3,7 @@
 # Find the process ID for port 3002
 port=3002
 #根据端口号查询对应的pid
-pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+pid=$(netstat -nlp | grep :3002 | awk '{print $7}' | awk -F"/" '{ print $1 }');
 
 #杀掉对应的进程，如果pid不存在，则不执行
 if [  -n  "$pid"  ];  then
